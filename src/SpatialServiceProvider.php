b745e675b00d836c1de4a6a4449b317f85a222a0
Michel Tomas <michel@kissmy.co>
2023-12-07 11:12:54 +0100

    Add Octane compatibility

--- src/SpatialServiceProvider.php
+++ src/SpatialServiceProvider.php
@@ -30,15 +30,15 @@ class SpatialServiceProvider extends DatabaseServiceProvider
         // The connection factory is used to create the actual connection instances on
         // the database. We will inject the factory into the manager so that it may
         // make the connections while they are actually needed and not of before.
-        $this->app->singleton('db.factory', function ($app) {
-            return new ConnectionFactory($app);
+        $this->app->singleton('db.factory', function () {
+            return new ConnectionFactory(app());
         });
 
         // The database manager is used to resolve various connections, since multiple
         // connections might be managed. It also implements the connection resolver
         // interface which may be used by other components requiring connections.
-        $this->app->singleton('db', function ($app) {
-            return new DatabaseManager($app, $app['db.factory']);
+        $this->app->singleton('db', function () {
+            return new DatabaseManager(app(), app()['db.factory']);
         });
 
         if (class_exists(DoctrineType::class)) {
